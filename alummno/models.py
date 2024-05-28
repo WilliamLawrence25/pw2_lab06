70179e697da6d659a0fdc94abbcf78cc7268cf62
wchoquehuancab <wchoquehuancab@unsa.edu.pe>
2024-05-28 09:50:45 -0500

    add the clases Curso and NotasPorCurso in the models.py file

--- alummno/models.py
+++ alummno/models.py
@@ -8,3 +8,18 @@ class Alumno(models.Model):
     
     def __str__(self):
         return f"{self.name} {self.lastaname}"
+
+class Curso(models.Model):
+    name = models.CharField(max_length=100)
+    teacher_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name} {self.teacher_name}"
+    
+class NotasPorCurso(models.Model):
+    alumno = models.ForeignKey(Alumno, on_delete=models.CASCADE)
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    nota = models.DecimalField(max_digits=4, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.alumno} - {self.curso} - {self.nota}"
