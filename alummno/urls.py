2ba8254eed3fbd08f12a43c4fc9f829ea613f4fa
Yenaro Joel Noa Camino <ynoacamino@gmail.com>
2024-05-28 21:42:56 -0500

    Refactor models and views in alummno app

--- alummno/urls.py
+++ alummno/urls.py
@@ -1,6 +1,34 @@
+# alumno/urls.py
 from django.urls import path
-from . import views
+from .views import (
+    AlumnoListView,
+    AlumnoCreateView,
+    AlumnoUpdateView,
+    AlumnoDeleteView,
+    AlumnoDetailView,
+    CursoListView,
+    CursoCreateView,
+    CursoUpdateView,
+    CursoDeleteView,
+    CursoDetailView,
+)
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('alumnos/', AlumnoListView.as_view(), name='alumno-list'),
+    path('alumnos/new/', AlumnoCreateView.as_view(), name='alumno-create'),
+    path('alumnos/<int:pk>/', AlumnoDetailView.as_view(), name='alumno-detail'),
+    path('alumnos/<int:pk>/edit/', AlumnoUpdateView.as_view(), name='alumno-edit'),
+    path('alumnos/<int:pk>/delete/', AlumnoDeleteView.as_view(), name='alumno-delete'),
+    
+    path('cursos/', CursoListView.as_view(), name='curso-list'),
+    path('cursos/new/', CursoCreateView.as_view(), name='curso-create'),
+    path('cursos/<int:pk>/', CursoDetailView.as_view(), name='curso-detail'),
+    path('cursos/<int:pk>/edit/', CursoUpdateView.as_view(), name='curso-edit'),
+    path('cursos/<int:pk>/delete/', CursoDeleteView.as_view(), name='curso-delete'),
+
+    # path('notas/', NotasPorCursoListView.as_view(), name='notas-list'),
+    # path('notas/new/', NotasPorCursoCreateView.as_view(), name='notas-create'),
+    # path('notas/<int:pk>/', NotasPorCursoDetailView.as_view(), name='notas-detail'),
+    # path('notas/<int:pk>/edit/', NotasPorCursoUpdateView.as_view(), name='notas-edit'),
+    # path('notas/<int:pk>/delete/', NotasPorCursoDeleteView.as_view(), name='notas-delete'),
 ]
--- alummno/views.py
+++ alummno/views.py
@@ -1,7 +1,65 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+# alummno/views.py
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Alumno, Curso, NotasPorCurso
+from .forms import AlumnoForm, CursoForm, NotasPorCursoForm
 
-# Create your views here.
-def index(resquest):
+class AlumnoListView(ListView):
+    model = Alumno
+    template_name = 'alummno/alumno_list.html'
 
-    return HttpResponse("Hola awdadawda!")
+class AlumnoDetailView(DetailView):
+    model = Alumno
+    template_name = 'alummno/alumno_detail.html'
+
+class AlumnoCreateView(CreateView):
+    model = Alumno
+    form_class = AlumnoForm
+    template_name = 'alummno/alumno_form.html'
+    success_url = reverse_lazy('alumno-list')
+
+class AlumnoUpdateView(UpdateView):
+    model = Alumno
+    form_class = AlumnoForm
+    template_name = 'alummno/alumno_form.html'
+    success_url = reverse_lazy('alumno-list')
+
+class AlumnoDeleteView(DeleteView):
+    model = Alumno
+    template_name = 'alummno/alumno_confirm_delete.html'
+    success_url = reverse_lazy('alumno-list')
+
+class CursoListView(ListView):
+    model = Curso
+    template_name = 'alummno/curso_list.html'
+
+class CursoDetailView(DetailView):
+    model = Curso
+    template_name = 'alummno/curso_detail.html'
+
+class CursoCreateView(CreateView):
+    model = Curso
+    form_class = CursoForm
+    template_name = 'alummno/curso_form.html'
+    success_url = reverse_lazy('curso-list')
+
+class CursoUpdateView(UpdateView):
+    model = Curso
+    form_class = CursoForm
+    template_name = 'alummno/curso_form.html'
+    success_url = reverse_lazy('curso-list')
+
+class CursoDeleteView(DeleteView):
+    model = Curso
+    template_name = 'alummno/curso_confirm_delete.html'
+    success_url = reverse_lazy('curso-list')
+
+# class NotasPorCursoListView(ListView):
+#     model = NotasPorCurso
+#     template_name = 'alummno/notasporcurso_list.html'
+
+# class NotasPorCursoDetailView(DetailView):
+#     model = NotasPorCurso
+#     template_name = 'alummno/notasporcurso_detail.html'
